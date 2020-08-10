18f1e118b0667778adcb466bb3d674e07c1fcce8
meng <44870854+guohaomeng@users.noreply.github.com>
2020-08-10 10:55:51 +0800

    http://·····
    ┭┮﹏┭┮

--- autoload.js
+++ autoload.js
@@ -35,7 +35,7 @@ if (screen.width >= 768) {
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
 			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
-			cdnPath: "49.233.131.188:8086/"
+			cdnPath: "http://49.233.131.188:8086/"
 		});
 	});
 }
