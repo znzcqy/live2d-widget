5aee247d7d8a9ee808722e8e6ef7192ce5a058a3
meng <44870854+guohaomeng@users.noreply.github.com>
2020-08-10 09:33:49 +0800

    更改配置文件路径及后端api路径
    如题

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/guohaomeng/live2d-widget@latest/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -34,7 +34,8 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
+			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
+			cdnPath: "49.233.131.188:8086/"
 		});
 	});
 }
