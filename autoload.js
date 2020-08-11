1f1cde436de1d63a15211c8ebfdc538f0afbfc32
guohaomeng <3020721699@qq.com>
2020-08-11 18:58:25 +0800

    为GitHub pages博客使用进行优化5

--- autoload.js
+++ autoload.js
@@ -33,7 +33,7 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "https://live2d.fghrsh.net/",
+			apiPath: "https://live2d.fghrsh.net/api/",
 			//apiPath: "http://49.233.131.188:8086/",
 			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
 			cdnPath: ""
